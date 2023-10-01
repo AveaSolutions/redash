7c63358d32805058513984601809b1731813c3a7
Christopher Elliott <christopher@aveasolutions.com>
2023-10-01 11:42:41 -0700

    Include what the error was in the exception

--- redash/query_runner/mysql.py
+++ redash/query_runner/mysql.py
@@ -134,7 +134,7 @@ class Mysql(BaseSQLQueryRunner):
         results, error = self.run_query(query, None)
 
         if error is not None:
-            raise Exception("Failed getting schema.")
+            raise Exception("Failed getting schema. " + error)
 
         results = json_loads(results)
 
