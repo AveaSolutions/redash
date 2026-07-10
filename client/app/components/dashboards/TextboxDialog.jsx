cfa0e287813f37a1d5fb4116effc67d8001c78ac
jdeal-avea <jeremy.deal@kipuhealth.com>
2026-07-10 14:35:21 -0400

    AO-19393: Replace markdown with marked
    Swap abandoned markdown@0.5.0 for marked@9.1.6 behind a small toHTML
    wrapper. All rendered output still passes through HtmlContent/DOMPurify.

--- client/app/components/dashboards/TextboxDialog.jsx
+++ client/app/components/dashboards/TextboxDialog.jsx
@@ -1,5 +1,5 @@
 import { toString } from "lodash";
-import { markdown } from "markdown";
+import { toHTML as markdownToHTML } from "@/services/markdown";
 import React, { useState, useEffect, useCallback } from "react";
 import PropTypes from "prop-types";
 import { useDebouncedCallback } from "use-debounce";
@@ -20,11 +20,11 @@ function TextboxDialog({ dialog, isNew, ...props }) {
 
   useEffect(() => {
     setText(props.text);
-    setPreview(markdown.toHTML(props.text));
+    setPreview(markdownToHTML(props.text));
   }, [props.text]);
 
   const [updatePreview] = useDebouncedCallback(() => {
-    setPreview(markdown.toHTML(text));
+    setPreview(markdownToHTML(text));
   }, 200);
 
   const handleInputChange = useCallback(
